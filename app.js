e9f896e5c707d48fc04c5a679e041faa7cd47eb2
BoburXO <97375635+BoburXO@users.noreply.github.com>
2022-01-15 15:36:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const x = document.getElementById("ixlas");
+const f = document.getElementById("falaq");
+const n =document.getElementById("an-nas");
+
+function playSurah() {
+    x.play();
+}
+function pauseSurah() {
+    x.pause();
+}
+
+
+function playFalaq() {
+    f.play();
+}
+function pauseFalaq(){
+    f.pause();
+}
+
+
+function playNas() {
+    n.play();
+}
+function pauseNas(){
+    n.pause();
+}
